b9f5689ddf017f0943796d1427c304d88cab2b1c
Jiajie Zhang <zhangjiajie043@gmail.com>
2017-06-26 03:47:08 +0800

    randseed 32, score 893

--- tetris_ai.py
+++ tetris_ai.py
@@ -108,6 +108,6 @@ class TetrisAI(object):
 
         absDy = sum([abs(x) for x in roofDy])
 
-        return fullLines * 3 + nearFullLines * 1.2 - vHoles * 2.8 - hHoles * 0.0 - vBlocks * 0.1 - maxHeight * 0.02 \
-            - stdY * 0.0 - stdDY * 0.08 - absDy * 0.2
+        return fullLines * 3 + nearFullLines * 1.4 - vHoles * 1.8 - hHoles * 0.0 - vBlocks * 0.1 - maxHeight * 0.05 \
+            - stdY * 0.0 - stdDY * 0.05 - absDy * 0.2
 
